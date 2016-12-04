c09b1fe4f095e1e01fc4b0a2a7443337073749a1
AxelZeroLord <raphael_spain@hotmail.com>
2016-12-03 20:03:22 -0400

    Revert "added comment and deleted codecodecode"
    This reverts commit 5c90716e0655f80dc1197ac5a173e6d078028170.

--- Travel_Agency/src/interfaces/Insert.java
+++ Travel_Agency/src/interfaces/Insert.java
@@ -1,5 +1,5 @@
 package interfaces;
-
+codecodecode
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -34,7 +34,6 @@ public class Insert {
 	
 	public void test(){
 		System.out.println("Hello World");
-		//asco
 	}
 	
 	
